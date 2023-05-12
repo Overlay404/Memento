a1af2b87d22e0b4e749974a9e6b116dc654c7fd1
Overlay404 <denis.alekseev.200411@gmail.com>
2023-05-12 13:16:59 +0300

    Добавьте файлы проекта.

---
+++ Memento/View/Controls/TextBlockCustom.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Memento.View.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для TextBloclCustom.xaml
+    /// </summary>
+    public partial class TextBlockCustom : UserControl
+    {
+
+
+        public string TextInTextBlock
+        {
+            get { return (string)GetValue(TextInTextBlockProperty); }
+            set { SetValue(TextInTextBlockProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextInTextBlockProperty =
+            DependencyProperty.Register("TextInTextBlock", typeof(string), typeof(TextBlockCustom));
+
+
+        public TextBlockCustom()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Memento/View/Windows/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Memento.View.Pages;
+using System.Windows;
+
+namespace Memento.View.Windows
+{
+    /// <summary>
+    /// Основное окно с элементом Frame
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static MainWindow Instance { get; private set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Instance = this;
+            MainFrame.Navigate(new AutorizationPage());
+        }
+    }
+}
