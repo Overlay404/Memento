1fb2d1526c22a6a348995c1bd7e01f2c876ca1ff
Overlay404 <denis.alekseev.200411@gmail.com>
2023-05-15 12:30:37 +0300

    GroupVisitPage Complited

--- Memento/View/Pages/GroupVisitPage.xaml.cs
+++ Memento/View/Pages/GroupVisitPage.xaml.cs
@@ -31,13 +31,61 @@ namespace Memento.View.Pages
         }
 
         public static readonly DependencyProperty NameFileProperty =
-            DependencyProperty.Register("NameFileTry", typeof(string), typeof(PersonVisitPage));
+            DependencyProperty.Register("NameFileTry", typeof(string), typeof(GroupVisitPage));
+
+
+
+
+        public List<Visitor> Visitors
+        {
+            get { return (List<Visitor>)GetValue(VisitorsProperty); }
+            set { SetValue(VisitorsProperty, value); }
+        }
+
+        public static readonly DependencyProperty VisitorsProperty =
+            DependencyProperty.Register("Visitors", typeof(List<Visitor>), typeof(GroupVisitPage));
+
+
+        public IEnumerable<Division> Divisions
+        {
+            get { return (IEnumerable<Division>)GetValue(DivisionsProperty); }
+            set { SetValue(DivisionsProperty, value); }
+        }
+
+        public static readonly DependencyProperty DivisionsProperty =
+            DependencyProperty.Register("Divisions", typeof(IEnumerable<Division>), typeof(GroupVisitPage));
+
+
+        public IEnumerable<VisitPurpose> VisitPurposes
+        {
+            get { return (IEnumerable<VisitPurpose>)GetValue(VisitPurposesProperty); }
+            set { SetValue(VisitPurposesProperty, value); }
+        }
+
+        public static readonly DependencyProperty VisitPurposesProperty =
+            DependencyProperty.Register("VisitPurposes", typeof(IEnumerable<VisitPurpose>), typeof(GroupVisitPage));
+
+        public IEnumerable<Employee> Employee
+        {
+            get { return (IEnumerable<Employee>)GetValue(EmployeeProperty); }
+            set { SetValue(EmployeeProperty, value); }
+        }
+
+        public static readonly DependencyProperty EmployeeProperty =
+            DependencyProperty.Register("Employee", typeof(IEnumerable<Employee>), typeof(GroupVisitPage));
+
+
 
         Regex emailRegex = new Regex(@"([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)");
         Regex phoneRegex = new Regex(@"^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$");
 
         public GroupVisitPage()
         {
+            Divisions = Connection.db.Division.ToList();
+            Employee = Connection.db.Employee.ToList();
+            VisitPurposes = Connection.db.VisitPurpose.ToList();
+
+            Visitors = new List<Visitor>();
             InitializeComponent();
 
             AttachFile.MouseDown += (sender, e) =>
@@ -45,41 +93,66 @@ namespace Memento.View.Pages
                 AttachFile.Visibility = Visibility.Collapsed;
                 NameFileTry = OpenFileDialogSave();
             };
+            AddingVisitor.MouseDown += (sender, e) =>
+            {
+                try
+                {
+                    VisitPurpose cbVisit = VisitPurposeCB.SelectedItem as VisitPurpose;
+                    Division cbDivision = DivisionCB.SelectedItem as Division;
+                    Employee cbEmpl = EmployeeCB.SelectedItem as Employee;
+
+                    if (!Validate(emailRegex, phoneRegex, cbVisit, cbDivision, cbEmpl))
+                    {
+                        MessageBox.Show("Не введены или не соответсвуют формату данные");
+                    }
+
+                    VisitPurpose visitPurposeName = Connection.db.VisitPurpose.FirstOrDefault(v => v.Name == (cbVisit).Name);
+                    int divisionId = Connection.db.Division.FirstOrDefault(d => d.Name == (cbDivision).Name).Id;
+                    int employeeId = Connection.db.Employee.FirstOrDefault(em => em.LastName == (cbEmpl).LastName).Id;
+
+
+                    Visitor visitor = new Visitor
+                    {
+                        LastName = SurnameInForm.TextInTextBox,
+                        FirstName = NameInForm.TextInTextBox,
+                        Patronymic = PatronymicnForm.TextInTextBox,
+                        Email = Mail.TextInTextBox == null ? null : Mail.TextInTextBox.Trim(), 
+                        Phone = Phone.TextInTextBox == null ? null : Phone.TextInTextBox.Trim(),
+                        OrganizationId = 1,
+                        Note = Note.TextInTextBox,
+                        PassportSeries = Series.TextInTextBox,
+                        PassportNumber = Number.TextInTextBox,
+                        BirthDate = DateTime.Today,
+                    };
+
+                    Request request = new Request
+                    {
+                        RequestTypeId = 1,
+                        RequestStatusId = 1,
+                        DesiredStartDate = WithDatePicker.SelectedDate ?? DateTime.Today,
+                        DesiredExpirationDate = ByDatePicker.SelectedDate ?? DateTime.Today,
+                        VisitPurpose = visitPurposeName,
+                        DivisionId = divisionId,
+                        EmployeeId = employeeId
+                    };
+
+                    request.Visitor.Add(visitor);
+
+                    Connection.db.Request.Add(request);
+
+                    Connection.db.Visitor.Add(visitor);
+
+                    Visitors.Add(visitor);
+                    ListVisitor.ItemsSource = Visitors;
+                    ListVisitor.Items.Refresh();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Не заполнены все основные поля");
+                }
+            };
             CreateRequestBtn.Click += (sender, e) =>
             {
-                VisitPurpose cbVisit = VisitPurposeCB.SelectedItem as VisitPurpose;
-                Division cbDivision = DivisionCB.SelectedItem as Division;
-                Employee cbEmpl = EmployeeCB.SelectedItem as Employee;
-
-                if (Validate(emailRegex, phoneRegex, cbVisit, cbDivision, cbEmpl))
-                {
-                    MessageBox.Show("Не введены или не соответсвуют формату данные");
-                }
-
-                VisitPurpose visitPurposeName = Connection.db.VisitPurpose.FirstOrDefault(v => v.Name == (cbVisit).Name);
-                int divisionId = Connection.db.Division.FirstOrDefault(d => d.Name == (cbDivision).Name).Id;
-                int employeeId = Connection.db.Employee.FirstOrDefault(em => em.LastName == (cbEmpl).LastName).Id;
-
-
-                Connection.db.Request.Add(new Request
-                {
-                    RequestTypeId = 1,
-                    RequestStatusId = 1,
-                    DesiredStartDate = WithDatePicker.SelectedDate ?? DateTime.Today,
-                    DesiredExpirationDate = ByDatePicker.SelectedDate ?? DateTime.Today,
-                    VisitPurpose = visitPurposeName,
-                    DivisionId = divisionId,
-                    EmployeeId = employeeId
-                });
-
-                Connection.db.Visitor.Add(new Visitor
-                {
-                    LastName = Surname.TextInTextBox.Trim(),
-                    FirstName = Name.TextInTextBox.Trim(),
-                    Patronymic = Patronymic.TextInTextBox.Trim(),
-                    BirthDate = DateTime.Today,
-                });
-
                 try
                 {
                     Connection.db.SaveChanges();
@@ -99,15 +172,14 @@ namespace Memento.View.Pages
                     MessageBox.Show(string.Join("\n", mess));
                 }
 
-                MessageBox.Show($"Заявка на имя {Surname.TextInTextBox} {Name.TextInTextBox} {Patronymic.TextInTextBox} заполнена");
+                MessageBox.Show($"Заявка на имя {SurnameInForm.TextInTextBox} {NameInForm.TextInTextBox} {PatronymicnForm.TextInTextBox} заполнена");
             };
         }
 
         private bool Validate(Regex emailRegex, Regex phoneRegex, VisitPurpose cbVisit, Division cbDivision, Employee cbEmpl)
         {
             if (cbVisit == null || cbDivision == null || cbEmpl == null) return false;
-            if (string.IsNullOrEmpty(Surname.TextInTextBox) || string.IsNullOrEmpty(Name.TextInTextBox) || string.IsNullOrEmpty(Patronymic.TextInTextBox)) return false;
-            if (emailRegex.IsMatch(Mail.TextInTextBox.Trim()) || phoneRegex.IsMatch(Phone.TextInTextBox.Trim())) return false;
+            if (string.IsNullOrEmpty(SurnameInForm.TextInTextBox) || string.IsNullOrEmpty(NameInForm.TextInTextBox) || string.IsNullOrEmpty(PatronymicnForm.TextInTextBox)) return false;
             return true;
         }
 
